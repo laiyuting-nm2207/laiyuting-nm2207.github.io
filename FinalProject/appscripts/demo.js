678a0c4ea5b60465a7bd4064c35ec488d7ae8afc
laiyuting-nm2207 <125462467+laiyuting-nm2207@users.noreply.github.com>
2023-03-26 23:26:29 +0800

    week 10 final project submissions

---
+++ FinalProject/appscripts/demo.js
@@ -0,0 +1,20 @@
+var TrialCircle = document.getElementById("Trial");
+var TC = TrialCircle.getContext("2d");
+TC.beginPath();
+TC.arc(200, 200, 100, 0, 2 * Math.PI);
+TC.fillStyle = 'yellow';
+TC.linewidth = 15;
+TC.strokeStyle = 'black';
+TC.stroke();
+
+function hideReady (){
+    document.getElementById("YouAreReady").style.visibility = "hidden";
+}
+
+function GoToPreGameFlash () {
+    Trial.addEventListener("click", function (YouAreReady){
+    document.getElementById("YouAreReady").style.visibility = "visible";
+    });
+};
+
+// got help from: https://www.tutorialspoint.com/How-to-set-whether-an-element-should-be-visible-in-JavaScript#:~:text=style%5B%22visibility%22%5D%20%3D,should%20be%20visible%20or%20not.
--- FinalProject/appscripts/main.js
+++ FinalProject/appscripts/main.js
@@ -1,11 +1,15 @@
 
 
-// pause auto music for starting screen
-var myMusic = document.getElementById("music");
-function play() {
-    MyMusic.pause();
-};
+// click to pause music for starting screen
+const audio = document.getElementById("music");
 
+// PauseMusic.addEventListener("click", function (ev) {
+// document.getElementById("music") = false;
+
+// });
+// ^ doesnt work yet... still dk how
+
+// click to unpause music
 
 // page 2 input username
 const UN = document.getElementById('username');
@@ -14,9 +18,70 @@ insertName.addEventListener("submit", function (e) {
     e.preventDefault(); // to prevent the page from refreshing
     const usernameValue = UN.value;
     localStorage.setItem('user-name', usernameValue);
-    window.location.href="Instructions.html";
+    window.location.href = "Instructions.html";
 });
 
 // page 3 instructions
 
 // page 4 trial page
+
+// ^ refer to  demo.js
+
+// page 5 pre-game flash
+
+setTimeout(function (flash3) {
+    document.getElementById("3").style.visbility = 'visible';
+    document.getElementById("2").style.visbility = 'hidden';
+    document.getElementById("1").style.visbility = 'hidden';
+    document.getElementById("startRealGame").style.visbility = 'hidden';
+}, 1000);
+
+setTimeout(function (flash2) {
+    document.getElementById("3").style.visbility = 'hidden';
+    document.getElementById("2").style.visbility = 'visible';
+    document.getElementById("1").style.visbility = 'hidden';
+    document.getElementById("startRealGame").style.visbility = 'hidden';
+}, 1000);
+
+setTimeout(function (flash1) {
+    document.getElementById("3").style.visbility = 'hidden';
+    document.getElementById("2").style.visbility = 'hidden';
+    document.getElementById("1").style.visbility = 'visible';
+    document.getElementById("startRealGame").style.visbility = 'hidden';
+}, 1000);
+
+setTimeout(function (flashstart) {
+    document.getElementById("3").style.visbility = 'hidden';
+    document.getElementById("2").style.visbility = 'hidden';
+    document.getElementById("1").style.visbility = 'hidden';
+    document.getElementById("startRealGame").style.visbility = 'visible';
+}, 1000);
+
+// ^ the flash doesnt work T^T
+
+
+// page 6 actual game
+document.getElementById("timesUp").style.visibility='hidden';
+let points = 0
+clicked = document.addEventListener("click", function (score) {
+    points = 0++;
+})
+
+const lettertoajax = document.querySelector("audio");
+
+audio.addEventListener("ended", (timesUp) => {
+  document.getElementById("timesUp").style.visibility='visible';
+});
+
+// page 7 time's up screen
+
+
+// page 8 score summary
+// find out what total is
+let missed = total - points;
+let percent = (points * 100) / total;
+
+document.getElementById("scores").innerHTML = "Scored:" + points
+document.getElementById("missed").innerHTML = "Missed:" + missed;
+document.getElementById("overall").innerHTML = "Overall:" + percent;
+
